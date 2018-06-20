016616ae05c42add993ef4d96e6434dd68f722a8
崔志 <cuizhi@leoao.com>
2018-06-20 17:18:26 +0800

    增加一个字段

--- src/main/java/com/czz/ioc/test1.java
+++ src/main/java/com/czz/ioc/test1.java
@@ -4,4 +4,13 @@ package com.czz.ioc;
  * Create by czz on 2018/6/20 15:09
  **/
 public class test1 {
+    private Integer id;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
 }
